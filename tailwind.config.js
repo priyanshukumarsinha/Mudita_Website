be4f60d710aa052b45cb9cf7d59f9487e4a84d11
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-10-04 04:36:59 +0000

    initial_setup && tailwind_configuration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors : {
+        bgColor : "#28323E",
+        primaryColor : "#D0A676",
+        secondaryColor : "#D9D9D9",
+        bgDim : "#28323ECC",
+      }
+    },
+  },
+  plugins: [],
+}
+
