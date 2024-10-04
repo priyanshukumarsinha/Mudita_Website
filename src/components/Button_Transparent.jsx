b174fe57968bd9551df50c2e46e9469df09eb2ca
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-10-04 04:43:03 +0000

    Navbar_initialization

---
+++ src/components/Button_Transparent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button_Transparent = () => {
+  return (
+    <div>Button_Transparent</div>
+  )
+}
+
+export default Button_Transparent
---
+++ src/components/Nabvar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+import Menu from './Menu'
+import Button_Transparent from './Button_Transparent'
+
+const Nabvar = () => {
+  return (
+    <div className='w-full'>
+        <Logo />
+        <Menu />
+        <Button_Transparent />
+    </div>
+  )
+}
+
+export default Nabvar
